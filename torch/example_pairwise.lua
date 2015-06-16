2d95407ad474c93edaf0876b3736965d9f04616c
Alex Rush <sasha.rush@gmail.com>
2015-06-16 08:27:21 -0400

    result hdf5

--- torch/example_pairwise.lua
+++ torch/example_pairwise.lua
@@ -12,11 +12,13 @@ cmd:text()
 train.config(cmd)
 tdnn.config(cmd)
 
+cmd:option('-test', false)
 cmd:option('-trainData', 'n', 'training data file')
 cmd:option('-epochs', 1, 'epochs')
 cmd:option('-labels', 10, 'labels')
 cmd:option('-cuda', 1, 'use gpu')
 cmd:option('-dropoutP', 0.5, 'dropout rate')
+cmd:option('-network', 1)
 params = cmd:parse(arg)
 
 function make_network(config, w2v)
@@ -26,21 +28,35 @@ function make_network(config, w2v)
 
    local network1 = tdnn.build(params, w2v)
    local network2 = tdnn.build(params, w2v)
-   local network3 = tdnn.build_pairwise(params, w2v)
+
+   local features = nn.LookupTable(24000, H)
    
    local u = {network1(input1),
               network2(input2),
-              network3(input3)}
+              nn.Sum(2)(features)
+   }
    
    local pen = nn.JoinTable(2)(u)
    local H = config.hiddenSize
 
    local penultimate2 = nn.ReLU()(
-      nn.Linear(7*H, H)(nn.Dropout(config.dropoutP)(pen)))
+      nn.Linear(7*H, 3*H)(nn.Dropout(config.dropoutP)(pen)))
+   local penultimate_drop = nn.Dropout(config.dropoutP)(penultimate2)
+   local output = nn.LogSoftMax()(nn.Linear(3*H, params.labels)(penultimate_drop))
+   return nn.gModule({input1, input2}, {output})
+end
+
+function make_network2(config, w2v)
+   local input3 = nn.Identity()()
+   local network3 = tdnn.build_pairwise(params, w2v)
+   
+   local H = config.hiddenSize
+
+   local penultimate2 = nn.ReLU()(
+      nn.Linear(H, H)(nn.Dropout(config.dropoutP)(network3(input3))))
    local penultimate_drop = nn.Dropout(config.dropoutP)(penultimate2)
    local output = nn.LogSoftMax()(nn.Linear(H, params.labels)(penultimate_drop))
-   -- return nn.gModule({input1, input2}, {output})
-   return nn.gModule({input1, input2, input3}, {output})
+   return nn.gModule({input3}, {output})
 end
 
 local function make_cuda(vec)
@@ -52,32 +68,67 @@ local function make_cuda(vec)
 end
 
 local f = hdf5.open(params.trainData,'r')
+
+
+
+if params.network == 1 then
+   function g(data, i, size)
+      return {data[1]:narrow(1, i, size), 
+              data[2]:narrow(1, i, size)}
+   end
+else
+   function g(data, i, size)
+      a = align:narrow(1, i, size):view(size, 30*30*2)
+      return a                  
+   end
+end
+
+
+if params.test then 
+   local target_data = make_cuda(f:read('train_label'):all())--:narrow(1,1,100))
+   params.labels = target_data:max()
+
+   local network = torch.load("best_model.torch")
+   local criterion = nn.ClassNLLCriterion()
+   criterion:cuda()
+   local test_data = {}
+   test_data[1] = make_cuda(f:read('dev_arg1'):all())
+   test_data[2] = make_cuda(f:read('dev_arg2'):all())
+   local test_target_data = make_cuda(f:read('dev_label'):all())
+   local score, result = train.eval(network, criterion, 
+                                    test_data,
+                                    test_target_data, g, 
+                                    params)
+   local out = hdf5.open("result.hd5", 'w')
+   out:write("dev_result", torch.Tensor(result))
+else 
+
 local w2v = f:read('embeding'):all()
 
 local train_data = {}
 local test_data = {}
-train_data[1] = make_cuda(f:read('train_arg1'):all())
-train_data[2] = make_cuda(f:read('train_arg2'):all())
-local target_data = make_cuda(f:read('train_label'):all())
+train_data[1] = make_cuda(f:read('train_arg1'):all())--:all():narrow(1,1,100))
+train_data[2] = make_cuda(f:read('train_arg2'):all())--:narrow(1,1,100))
+local target_data = make_cuda(f:read('train_label'):all())--:narrow(1,1,100))
 params.labels = target_data:max()
 target_data = target_data
 
 local n1 = math.min(train_data[1]:size(2), 30)
 local n2 = math.min(train_data[2]:size(2), 30)
 
--- Alignment Matrix
-local align = torch.ones(train_data[1]:size(1), n1, n2, 2):long():fill(2)
+-- -- Alignment Matrix
+-- local align = torch.ones(train_data[1]:size(1), n1, n2, 2):long():fill(2)
 
-local n = 1
-for i = 1, n1 do 
-   for j = 1, n2 do 
-      align[{{}, i, j, 1}]:copy(train_data[1][{{}, i}])
-      n = n + 1
-      align[{{}, i, j, 2}]:copy(train_data[2][{{}, j}] + params.vocabSize) 
-      n = n + 1
-   end
-end
-
+-- local n = 1
+-- for i = 1, n1 do 
+--    for j = 1, n2 do 
+--       align[{{}, i, j, 1}]:copy(train_data[1][{{}, i}])
+--       n = n + 1
+--       align[{{}, i, j, 2}]:copy(train_data[2][{{}, j}] + params.vocabSize) 
+--       n = n + 1
+--    end
+-- end
+--- print(train_data[2][55])
 
 
 test_data[1] = make_cuda(f:read('dev_arg1'):all())
@@ -85,13 +136,17 @@ test_data[2] = make_cuda(f:read('dev_arg2'):all())
 local test_target_data = make_cuda(f:read('dev_label'):all())
 
 
-local criterion = nn.ClassNLLCriterion()
-network = make_network(params, w2v)
+
+if params.network == 1 then
+   network = make_network(params, w2v)
+else
+   network = make_network2(params, w2v)
+end
 
 if params.cuda~=0 then 
    network:cuda()
    criterion:cuda()
-   align = align:cuda()
+   -- align = align:cuda()
 end
 
 -- L2 Rescaling. 
@@ -124,20 +179,12 @@ local best_score = 1e10
 state = {
    learningRate = config.learningRate,
 }
-
 for i = 1, params.epochs do
    shuffle = torch.randperm(train_data[1]:size(1))
    train_data[1] = train_data[1]:index(1, shuffle:long())
    train_data[2] = train_data[2]:index(1, shuffle:long())
    target_data = target_data:index(1, shuffle:long())
 
-
-   local function g(data, i, size)
-      return {data[1]:narrow(1, i, size), 
-              data[2]:narrow(1, i, size),
-              align:narrow(1, i, size):view(size, 30*30*2)}
-   end
-   
    train.train(network, 
                criterion, 
                train_data,
@@ -151,4 +198,5 @@ for i = 1, params.epochs do
       print("BEST SCORE", score)
       torch.save("best_model.torch", network)
    end
+end
 end
--- torch/train.lua
+++ torch/train.lua
@@ -36,7 +36,6 @@ function train.train(network, criterion, data, target, g, rescale, config, state
       end
       optim.adadelta(func, x, state)
       rescale(x)
-      print("done")
       if n % 10 == 0 then
          print(n)
       end
@@ -59,19 +58,28 @@ function train.eval(network, criterion, data, target, g, config)
    confusion:zero()  
    local correct = 0
    sys.tic()
-   for i = 1, data[1]:size(1) - config.batchSize, config.batchSize do
-      local input = g(data, i, config.batchSize)
-      local targ = target:narrow(1, i, config.batchSize)
+   local result = {}
+   local n = data[1]:size(1)
+   for i = 1, n, config.batchSize do
+      local off = config.batchSize
+      if i + off > n then 
+         off = n - i
+      end
+      local input = g(data, i, off)
+      local targ = target:narrow(1, i, off)
       local out = network:forward(input)
-      loss = loss + criterion:forward(out, targ) * config.batchSize
-      total = total + config.batchSize
-      for j = 1, config.batchSize do 
-         confusion:add(out[j], targ[j])   
+      loss = loss + criterion:forward(out, targ) * off
+      total = total + off
+      
+      for j = 1, off do 
+         confusion:add(out[j], targ[j])
+         local a, i = out[j]:max(1)
+         table.insert(result, i[1])
       end
    end
    print(confusion)
    print("[EVAL loss=", loss/total, "time=", sys.toc(), "]")
-   return loss / total
+   return loss / total, result
 
 end
 
